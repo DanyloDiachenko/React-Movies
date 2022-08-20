083c7a4670cf49ff6a0e3651c803656a2cc80f6e
DanyloDiachenko <107757498+DanyloDiachenko@users.noreply.github.com>
2022-08-20 15:31:46 +0300

    Updates

---
+++ precache-manifest.037d88f1f910d01acb6229a8c5085e85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "537356c8318bbc4f1267",
+    "url": "/React-Movies/static/css/main.9089d038.chunk.css"
+  },
+  {
+    "revision": "537356c8318bbc4f1267",
+    "url": "/React-Movies/static/js/main.537356c8.chunk.js"
+  },
+  {
+    "revision": "0df596195c8b1a5b6683",
+    "url": "/React-Movies/static/js/1.0df59619.chunk.js"
+  },
+  {
+    "revision": "ef389ea6bc86ab858eba",
+    "url": "/React-Movies/static/js/runtime~main.ef389ea6.js"
+  },
+  {
+    "revision": "04446d14f7515bd9957ff249d8d7f205",
+    "url": "/React-Movies/index.html"
+  }
+];
